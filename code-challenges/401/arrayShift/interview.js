c33dc1fca4ab14f0aab9d94d201fd276d216a673
silan <siland.dope@yahoo.fr>
2020-03-30 21:42:16 -0700

    set up for interview challenges

---
+++ code-challenges/401/arrayShift/interview.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/* -write a function to add a each row in a matrix of arbitrary size
+  - return an array with value
+  -avoid using any bulding mathode()
+  - matrix array of arrays
+  -matrix always full of integer
+  -negative value are possible inside matrix
+  -all null will be counting as 0
+ ex: matrix = [
+   [1,2,1],
+   [2,3,5],
+   [5,6,2]
+ ]
+ result [4,10,13]
+
+*/
+/* Problem Domain:
+ write a function to add each row in a matrix of abitrary size.
+*/
+/*visual:
+ input: [
+   [1,2,3],
+   [3,5,7],
+   [1,7,10]
+
+  ]
+  output: [6,15,18]
+ */
+/**
+ algorithm:
+  write function addRow
+  variable matrix [[...],[....]]
+  loop through each array
+  return the sum of each array
+
+*/
+
+/*
+ pseudo code:
+  const addRow = () =>
+  const matrix = [
+   [1,2,3],
+   [3,5,7],
+   [1,7,10]
+  ]
+  
+ */
+
