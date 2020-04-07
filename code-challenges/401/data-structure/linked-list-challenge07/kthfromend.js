ddf08460a4fb33377c4d41133789336ef423fb69
silan <siland.dope@yahoo.fr>
2020-04-07 16:26:00 -0700

    in progress

---
+++ code-challenges/401/data-structure/linked-list-challenge07/kthfromend.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/*Write a method for the Linked List class which takes a number, k, as a parameter. Return the node’s value that is k from the end of the linked list. You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges.*/
+
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = LinkedList;
