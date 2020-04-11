d5d96489fe70d351c0303f1dae80902f3700c345
silan <siland.dope@yahoo.fr>
2020-04-11 06:58:55 -0700

    in progress

---
+++ code-challenges/401/data-structure/llmerge/ll-merge.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/*Write a function called mergeLists which takes two linked lists as arguments. Zip the two linked lists together into one so that the nodes alternate between the two lists and return a reference to the head of the zipped list. Try and keep additional space down to O(1). You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges*/
+
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
+
+module.exports = LinkedList;
