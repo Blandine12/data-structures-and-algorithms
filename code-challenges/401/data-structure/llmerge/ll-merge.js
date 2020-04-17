5dfef50abafac6c267a0cfa750b34bb9039bc517
silan <siland.dope@yahoo.fr>
2020-04-16 18:18:50 -0700

    fixed challenge

--- code-challenges/401/data-structure/llmerge/ll-merge.js
+++ code-challenges/401/data-structure/llmerge/ll-merge.js
@@ -3,18 +3,64 @@
 /*Write a function called mergeLists which takes two linked lists as arguments. Zip the two linked lists together into one so that the nodes alternate between the two lists and return a reference to the head of the zipped list. Try and keep additional space down to O(1). You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges*/
 
 
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-    this.prev = null;
-  }
-}
+const LinkedList = require('../linked-list-challenge06/linkedlist.js');
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-}
 
-module.exports = LinkedList;
+
+const mergeLists = (list1, list2) => {
+
+  let mergedList = new LinkedList();
+  let l1_tracker = list1;
+  let l2_tracker = list2;
+
+
+  while (l1_tracker || l2_tracker) {
+
+    if (l1_tracker) {
+      mergedList.append(l1_tracker.val);
+      l1_tracker = l1_tracker.next;
+    }
+
+    if (l2_tracker) {
+      mergedList.append(l2_tracker.val);
+      l2_tracker = l2_tracker.next;
+    }
+  }
+  return mergedList.head;
+};
+
+const mergeListsO1 = (list1, list2) => {
+
+  let l1_tracker = list1;
+  let l2_tracker = list2;
+  let l3_tracker = null;
+  let l3_head = null;
+
+  while (l1_tracker || l2_tracker) {
+    if (l1_tracker) {
+      if (!l3_head) {
+        l3_tracker = l1_tracker;
+        l3_head = l1_tracker;
+      } else {
+        l3_tracker.next = l1_tracker;
+        l3_tracker = l3_tracker.next;
+      }
+      l1_tracker = l1_tracker.next;
+    }
+
+    if (l2_tracker) {
+      if (!l3_head) {
+        l3_tracker = l2_tracker;
+        l3_head = l2_tracker;
+      } else {
+        l3_tracker.next = l2_tracker;
+        l3_tracker = l3_tracker.next;
+      }
+      l2_tracker = l2_tracker.next;
+    }
+  }
+
+  return l3_head;
+};
+
+module.exports = { mergeLists, mergeListsO1 };
---
+++ code-challenges/401/data-structure/llmerge/readme.md
@@ -0,0 +1,26 @@
+# code challenge 08
+
+## DSA: Linked list insertions.
+
+### Author: Blandine Dasilveira
+
+### Links and Resources
+
+
+### Challenge
+This application will  implement a merge  Linked List.
+
+
+- [submission PR](https://github.com/Blandine12/data-structures-and-algorithms/pull/28)
+
+
+### Approach & Efficiency
+- Created two classes: Node and LinkedList
+- Created methods to merge 
+- Big-O:
+
+- .append(value): o(n)
+
+
+### Test
+- npm test llmerge
