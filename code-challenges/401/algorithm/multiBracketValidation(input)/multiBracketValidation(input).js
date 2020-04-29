d334b820990b322c8ff49008a18d3c2c75bcaaaa
silan <siland.dope@yahoo.fr>
2020-04-29 16:44:44 -0700

    fixed challenge

--- code-challenges/401/algorithm/multiBracketValidation(input)/multiBracketValidation(input).js
+++ code-challenges/401/algorithm/multiBracketValidation(input)/multiBracketValidation(input).js
@@ -5,3 +5,65 @@ Round Brackets : ()
 Square Brackets : []
 Curly Brackets : {} */
 
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    newNode.next = this.top;
+
+    this.top = newNode;
+  }
+
+  pop() {
+    if (!this.top) return null;
+
+    let tempNode = this.top;
+
+    this.top = tempNode.next;
+
+    tempNode.next = null;
+
+    return tempNode.val;
+  }
+
+
+}
+
+const validator = (str) => {
+  let chars = str.split('');
+  let bracketStack = new Stack();
+
+  for (let i = 0; i < chars.length; i++) {
+    if (chars[i] === '{' || chars[i] === '[' || chars[i] === '(') {
+      bracketStack.push(chars[i]);
+    } else if (chars[i] === '}' || chars[i] === ']' || chars[i] === ')') {
+      let poppedVal = bracketStack.pop();
+
+      if (chars[i] === '}' && poppedVal != '{'){
+        return false;
+      }
+      if (chars[i] === ']' && poppedVal != '['){
+        return false;
+      }
+      if (chars[i] === ')' && poppedVal != '(') {
+        return false;
+      }
+    }
+  }
+
+  if (bracketStack.top) return false;
+
+  return true;
+};
+
+module.exports = validator;
