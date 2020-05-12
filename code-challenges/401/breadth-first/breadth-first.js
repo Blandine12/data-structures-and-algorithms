c3f5031c4bfbe6c6e8890a30c85cdacceae7e865
silan <siland.dope@yahoo.fr>
2020-05-12 13:36:30 -0700

    fixed challenge

---
+++ code-challenges/401/breadth-first/breadth-first.js
@@ -0,0 +1,54 @@
+'use strict';
+
+class Node {
+  constructor(value = null, left = null, right = null) {
+    this.right = right;
+    this.left = left;
+    this.value = value;
+  }
+}
+
+
+class BinaryTree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  findMaximumValue() {
+    return this.preOrderIsh(this.root, -Infinity)
+  }
+  preOrderIsh(root, max) {
+    if(root.value > max) {
+      max = root.value;
+    }
+    if(root.left !== null) {
+      max = this.preOrderIsh(root.left, max);
+    }
+    if(root.right !== null) {
+      max = this.preOrderIsh(root.right, max)
+    }
+    return max;
+  }
+
+}
+
+function breadthFirst(tree) {
+  let queue = [];
+  const newArray = [];
+  let current;
+  queue.push(tree.root);
+  while(queue[0]) {
+    current = queue.shift();
+    newArray.push(current.value);
+    if(current.left !== null) {
+      queue.push(current.left);
+    }
+    if(current.right !== null) {
+      queue.push(current.right)
+    }
+  }
+  return newArray;
+}
+
+module.exports = { Node, BinaryTree, breadthFirst }
+
---
+++ code-challenges/401/breadth-first/readme.md
@@ -0,0 +1,28 @@
+
+
+# code challenge 17
+
+## DSA: .
+
+### Author: Blandine Dasilveira
+
+### Links and Resources
+
+
+### Challenge
+- 
+
+
+- [submission PR](https://github.com/Blandine12/data-structures-and-algorithms/pull/33)
+
+
+### Approach & Efficiency
+
+
+
+## Solution
+![alt text](./assets/breadthfirst.jpg)
+
+
+### Test
+- npm test breadth-first
